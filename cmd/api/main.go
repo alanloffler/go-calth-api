95c87550942eed0a8934e6f42a0ae884afdd750d
alanloffler <alanmatiasloffler@gmail.com>
2026-02-13 20:11:07 -0300

    feat(main): port by config file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"log"
+
+	"github.com/alanloffler/go-calth-api/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
 	var router *gin.Engine = gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Calth API running", "status": "success"})
 	})
 
-	router.Run(":3000")
+	router.Run(":" + cfg.Port)
 }
