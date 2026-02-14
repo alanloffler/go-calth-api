1348321edde8b9563428127787897b4f136c28f7
alanloffler <alanmatiasloffler@gmail.com>
2026-02-14 16:29:51 -0300

    feat(repository): user GetByID

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type UserRepository struct {
@@ -16,4 +17,8 @@ func NewUserRepository(q *sqlc.Queries) *UserRepository {
 
 func (r *UserRepository) Create(ctx context.Context, arg sqlc.CreateUserParams) (sqlc.User, error) {
 	return r.q.CreateUser(ctx, arg)
+}
+
+func (r *UserRepository) GetByID(ctx context.Context, id pgtype.UUID) (sqlc.User, error) {
+	return r.q.GetUserByID(ctx, id)
 }
