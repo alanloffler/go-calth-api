78cd88f02c0e0f4296ac96ccba424bd88b30863c
alanloffler <alanmatiasloffler@gmail.com>
2026-02-13 21:53:35 -0300

    feat(main): database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 
 	"github.com/alanloffler/go-calth-api/internal/config"
+	"github.com/alanloffler/go-calth-api/internal/database"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	// Load config
 	var cfg *config.Config
 	var err error
 	cfg, err = config.Load()
@@ -16,7 +19,19 @@ func main() {
 		log.Fatal("Failed to load configuration:", err)
 	}
 
+	// Database connection
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	defer pool.Close()
+
+	// Gin router
 	var router *gin.Engine = gin.Default()
+	router.SetTrustedProxies(nil)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Calth API running", "status": "success"})
