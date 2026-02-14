3fd9d45fcbce80e64b590b16e18b1e901674c09f
alanloffler <alanmatiasloffler@gmail.com>
2026-02-14 18:13:20 -0300

    feat(database): update businesses schema

--- internal/database/schema/schema.sql
+++ internal/database/schema/schema.sql
@@ -1,7 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- Stub tables referenced by users (expand later)
-
 CREATE TABLE roles (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
@@ -11,12 +9,24 @@ CREATE TABLE roles (
 
 CREATE TABLE businesses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(100) NOT NULL,
+    slug VARCHAR(50) NOT NULL,
+    tax_id VARCHAR(11) NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    trade_name VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    street VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    province VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(10) NOT NULL,
+    whatsapp_number VARCHAR(10),
+    website VARCHAR(100),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
 );
-
--- Users table
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
