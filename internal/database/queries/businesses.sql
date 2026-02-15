f113a5db04e6b56d20292a0c55766dbf201cbf79
alanloffler <alanmatiasloffler@gmail.com>
2026-02-15 13:05:55 -0300

    feat(database): UpdateBusiness query

--- internal/database/queries/businesses.sql
+++ internal/database/queries/businesses.sql
@@ -11,3 +11,22 @@ SELECT * FROM businesses;
 
 -- name: GetBusiness :one
 SELECT * FROM businesses WHERE id = $1;
+
+-- name: UpdateBusiness :one
+UPDATE businesses SET
+  slug = COALESCE(sqlc.narg('slug'), slug),
+  tax_id = COALESCE(sqlc.narg('tax_id'), tax_id),
+  company_name = COALESCE(sqlc.narg('company_name'), company_name),
+  trade_name = COALESCE(sqlc.narg('trade_name'), trade_name),
+  description = COALESCE(sqlc.narg('description'), description),
+  street = COALESCE(sqlc.narg('street'), street),
+  city = COALESCE(sqlc.narg('city'), city),
+  province = COALESCE(sqlc.narg('province'), province),
+  country = COALESCE(sqlc.narg('country'), country),
+  zip_code = COALESCE(sqlc.narg('zip_code'), zip_code),
+  email = COALESCE(sqlc.narg('email'), email),
+  phone_number = COALESCE(sqlc.narg('phone_number'), phone_number),
+  whatsapp_number = COALESCE(sqlc.narg('whatsapp_number'), whatsapp_number),
+  website = COALESCE(sqlc.narg('website'), website)
+WHERE id = $1
+RETURNING *;
