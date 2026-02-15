decb6b45fffe72447e3723ee37176f5361a80dff
alanloffler <alanmatiasloffler@gmail.com>
2026-02-15 11:39:49 -0300

    refactor(health): api response
    Replace gin.H with custom api response

--- internal/health/health_routes.go
+++ internal/health/health_routes.go
@@ -1,6 +1,9 @@
 package health
 
 import (
+	"net/http"
+
+	response "github.com/alanloffler/go-calth-api/internal/common"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -9,16 +12,15 @@ func RegisterRoutes(router *gin.Engine, pool *pgxpool.Pool) {
 	var health *gin.RouterGroup = router.Group("/health")
 
 	health.GET("", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Calth API running", "status": "success"})
+		c.JSON(http.StatusOK, response.Success[any]("Calth API running", nil))
 	})
 
 	health.GET("/db", func(c *gin.Context) {
 		if err := pool.Ping(c.Request.Context()); err != nil {
-			c.JSON(500, gin.H{"message": err.Error(), "status": "error"})
+			c.JSON(http.StatusInternalServerError, response.Error(http.StatusInternalServerError, "Database is not connected", err))
 			return
 		}
-		c.JSON(200, gin.H{
-			"message": "Database connected successfully", "status": "connected",
-		})
+
+		c.JSON(http.StatusOK, response.Success[any]("Database connected successfully", nil))
 	})
 }
