fa5582530ff612eac7a4aeef523538dda89dc6a4
alanloffler <alanmatiasloffler@gmail.com>
2026-02-14 21:29:11 -0300

    feat(business): handler

---
+++ internal/business/business_handler.go
@@ -0,0 +1,65 @@
+package business
+
+import (
+	"net/http"
+
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type BusinessHandler struct {
+	repo *BusinessRepository
+}
+
+func NewBusinessHandler(repo *BusinessRepository) *BusinessHandler {
+	return &BusinessHandler{repo: repo}
+}
+
+type CreateBusinessRequest struct {
+	Slug           string  `json:"slug" binding:"required"`
+	TaxId          string  `json:"taxId" binding:"required"`
+	CompanyName    string  `json:"companyName" binding:"required"`
+	TradeName      string  `json:"tradeName" binding:"required"`
+	Description    string  `json:"description" binding:"required"`
+	Street         string  `json:"street" binding:"required"`
+	City           string  `json:"city" binding:"required"`
+	Province       string  `json:"province" binding:"required"`
+	Country        string  `json:"country" binding:"required"`
+	ZipCode        string  `json:"zipCode" binding:"required"`
+	Email          string  `json:"email" binding:"required,email"`
+	PhoneNumber    string  `json:"phoneNumber" binding:"required"`
+	WhatsappNumber *string `json:"whatsappNumber"`
+	Website        *string `json:"website"`
+}
+
+func (h *BusinessHandler) Create(c *gin.Context) {
+	var req CreateBusinessRequest
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	business, err := h.repo.Create(c.Request.Context(), sqlc.CreateBusinessParams{
+		Slug:           req.Slug,
+		TaxID:          req.TaxId,
+		CompanyName:    req.CompanyName,
+		TradeName:      req.TradeName,
+		Description:    req.Description,
+		Street:         req.Street,
+		City:           req.City,
+		Province:       req.Province,
+		Country:        req.Country,
+		ZipCode:        req.ZipCode,
+		Email:          req.Email,
+		PhoneNumber:    req.PhoneNumber,
+		WhatsappNumber: pgtype.Text{String: "", Valid: req.WhatsappNumber != nil},
+		Website:        pgtype.Text{String: "", Valid: req.Website != nil},
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, business)
+}
