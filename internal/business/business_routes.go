4c1948bb86044d7944439144fa594f1ebfdf0afd
alanloffler <alanmatiasloffler@gmail.com>
2026-02-14 23:22:22 -0300

    feat(business): routes at domain

---
+++ internal/business/business_routes.go
@@ -0,0 +1,15 @@
+package business
+
+import (
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine, q *sqlc.Queries) {
+	var repo *BusinessRepository = NewBusinessRepository(q)
+	var handler *BusinessHandler = NewBusinessHandler(repo)
+	var businesses *gin.RouterGroup = router.Group("/businesses")
+
+	businesses.POST("", handler.Create)
+	businesses.GET("", handler.GetAll)
+}
