b7b3608f0e538f02835ddf7a6861fbbf73b55ab6
alanloffler <alanmatiasloffler@gmail.com>
2026-02-17 17:01:33 -0300

    refactor(database): GetRole -> GetRoleWithPermissions
    GetOneByID has relation with permissions

--- internal/database/queries/roles.sql
+++ internal/database/queries/roles.sql
@@ -16,9 +16,17 @@ ORDER BY value ASC;
 SELECT * FROM roles
 ORDER BY value ASC;
 
--- name: GetRole :one
-SELECT * FROM roles
-WHERE id = $1 AND deleted_at IS NULL;
+-- name: GetRoleWithPermissions :many
+SELECT
+   r.id, r.name, r.value, r.description, r.created_at, r.updated_at, r.deleted_at,
+   rp.role_id, rp.permission_id, rp.created_at AS rp_created_at, rp.updated_at AS rp_updated_at,
+   p.id AS p_id, p.name AS p_name, p.category AS p_category, p.action_key AS p_action_key,
+   p.description AS p_description, p.created_at AS p_created_at, p.updated_at AS p_updated_at,
+   p.deleted_at AS p_deleted_at
+FROM roles r
+LEFT JOIN role_permissions rp ON rp.role_id = r.id
+LEFT JOIN permissions p ON p.id = rp.permission_id
+WHERE r.id = $1 AND r.deleted_at IS NULL;
 
 -- name: GetRoleWithSoftDeleted :one
 SELECT * FROM roles
