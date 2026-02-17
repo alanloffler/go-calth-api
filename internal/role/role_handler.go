cd23267784add5bae78c986e26c71ff02ea3dc43
alanloffler <alanmatiasloffler@gmail.com>
2026-02-16 23:42:01 -0300

    feat(role): GetAllWithSoftDeleted method

--- internal/role/role_handler.go
+++ internal/role/role_handler.go
@@ -53,6 +53,16 @@ func (h *RoleHandler) GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, response.Success("Roles encontrados", &permissions))
 }
 
+func (h *RoleHandler) GetAllWithSoftDeleted(c *gin.Context) {
+	permissions, err := h.repo.GetAllWithSoftDeleted(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusNotFound, response.Error(http.StatusNotFound, "Roles no encontrados", err))
+		return
+	}
+
+	c.JSON(http.StatusOK, response.Success("Roles encontrados", &permissions))
+}
+
 func (h *RoleHandler) Delete(c *gin.Context) {
 	var id pgtype.UUID
 	if err := id.Scan(c.Param("id")); err != nil {
