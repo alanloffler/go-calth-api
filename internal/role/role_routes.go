48076ce2e37d5c7558f2bdc0245d499d83fbb921
alanloffler <alanmatiasloffler@gmail.com>
2026-02-16 21:20:52 -0300

    feat(role): create route

---
+++ internal/role/role_routes.go
@@ -0,0 +1,14 @@
+package role
+
+import (
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine, q *sqlc.Queries) {
+	var repo *RoleRepository = NewRoleRepository(q)
+	var handler *RoleHandler = NewRoleHandler(repo)
+	var roles *gin.RouterGroup = router.Group("/roles")
+
+	roles.POST("", handler.Create)
+}
