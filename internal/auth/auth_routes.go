38ed88a7a2a3b7f2453caaa33f05a8ed688de454
alanloffler <alanmatiasloffler@gmail.com>
2026-02-18 19:59:03 -0300

    fix(auth): auth service

--- internal/auth/auth_routes.go
+++ internal/auth/auth_routes.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
+	"github.com/alanloffler/go-calth-api/internal/config"
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(router *gin.Engine, q *sqlc.Queries) {
-	var service *AuthService
+func RegisterRoutes(router *gin.Engine, q *sqlc.Queries, cfg *config.Config) {
+	var service *AuthService = NewAuthService(cfg)
 	var repo *AuthRepository = NewAuthRepository(q)
 	var handler *AuthHandler = NewAuthHandler(service, repo)
 	var auth *gin.RouterGroup = router.Group("/auth")
