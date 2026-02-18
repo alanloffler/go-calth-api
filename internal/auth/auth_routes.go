ebeb018ac85cb433e8b78c2063695c600e7995cf
alanloffler <alanmatiasloffler@gmail.com>
2026-02-18 19:47:01 -0300

    feat(auth): routes

---
+++ internal/auth/auth_routes.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine, q *sqlc.Queries) {
+	var service *AuthService
+	var repo *AuthRepository = NewAuthRepository(q)
+	var handler *AuthHandler = NewAuthHandler(service, repo)
+	var auth *gin.RouterGroup = router.Group("/auth")
+
+	auth.POST("/login", handler.Login)
+	auth.POST("/logout", handler.Logout)
+	auth.POST("/refresh", handler.Refresh)
+}
