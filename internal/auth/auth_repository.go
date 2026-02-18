8ed840740dd3495d22db296cad18c246d776b1df
alanloffler <alanmatiasloffler@gmail.com>
2026-02-18 19:40:01 -0300

    feat(auth): repository

---
+++ internal/auth/auth_repository.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type AuthRepository struct {
+	q *sqlc.Queries
+}
+
+func NewAuthRepository(q *sqlc.Queries) *AuthRepository {
+	return &AuthRepository{q: q}
+}
+
+func (r *AuthRepository) GetUserByEmail(ctx context.Context, arg sqlc.GetUserByEmailParams) (sqlc.User, error) {
+	return r.q.GetUserByEmail(ctx, arg)
+}
+
+func (r *AuthRepository) GetUserByID(ctx context.Context, id pgtype.UUID) (sqlc.User, error) {
+	return r.q.GetUserByID(ctx, id)
+}
+
+func (r *AuthRepository) UpdateRefreshToken(ctx context.Context, arg sqlc.UpdateRefreshTokenParams) (sqlc.User, error) {
+	return r.q.UpdateRefreshToken(ctx, arg)
+}
+
+func (r *AuthRepository) ClearRefreshToken(ctx context.Context, id pgtype.UUID) (sqlc.User, error) {
+	return r.q.ClearRefreshToken(ctx, id)
+}
