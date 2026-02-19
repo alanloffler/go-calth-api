e715a0d4cc47fed356c9dc75a6d521f6f8ed6697
alanloffler <alanmatiasloffler@gmail.com>
2026-02-18 21:41:16 -0300

    feat(database): UpdateUser query

--- internal/database/queries/users.sql
+++ internal/database/queries/users.sql
@@ -11,6 +11,14 @@ INSERT INTO users (
     $1, $2, $3, $4, $5, $6, $7, $8, $9
 ) RETURNING *;
 
+-- name: UpdateUser :one
+UPDATE users
+SET ic = $2, user_name = $3, first_name = $4, last_name = $5,
+    email = $6, password = $7, phone_number = $8, role_id = $9,
+    updated_at = now()
+WHERE id = $1 AND deleted_at IS NULL
+RETURNING *;
+
 -- AUTH
 -- name: GetUserByEmail :one
 SELECT * FROM users
