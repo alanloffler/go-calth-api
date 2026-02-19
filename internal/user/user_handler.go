cb8b06bdeeb81cfbcf6bbcd4c3849df9e8d8bfcd
alanloffler <alanmatiasloffler@gmail.com>
2026-02-19 13:13:09 -0300

    feat(user): GetAll method

--- internal/user/user_handler.go
+++ internal/user/user_handler.go
@@ -85,6 +85,16 @@ func (h *UserHandler) Create(c *gin.Context) {
 	c.JSON(http.StatusCreated, response.Created("Usuario creado", &user))
 }
 
+func (h *UserHandler) GetAll(c *gin.Context) {
+	users, err := h.repo.GetAll(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusNotFound, response.Error(http.StatusNotFound, "Usuarios no encontrados", err))
+		return
+	}
+
+	c.JSON(http.StatusOK, response.Success("Usuarios encontrados", &users))
+}
+
 func (h *UserHandler) GetByID(c *gin.Context) {
 	var id pgtype.UUID
 	if err := id.Scan(c.Param("id")); err != nil {
