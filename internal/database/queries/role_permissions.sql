c87f39c8db23f8ab916383970e20191719268884
alanloffler <alanmatiasloffler@gmail.com>
2026-02-21 21:57:25 -0300

    feat(database): HasPermission query

--- internal/database/queries/role_permissions.sql
+++ internal/database/queries/role_permissions.sql
@@ -9,3 +9,10 @@ RETURNING *;
 
 -- name: DeleteRolePermissionsByRoleID :exec
 DELETE FROM role_permissions WHERE role_id = $1;
+
+-- name: HasPermission :one
+SELECT EXISTS (
+    SELECT 1 FROM role_permissions rp
+    JOIN permissions p ON p.id = rp.permission_id
+    WHERE rp.role_id = $1 AND p.action_key = $2
+) AS has_permission;
