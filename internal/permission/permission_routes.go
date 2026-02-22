6e3272782c3b64e94be27182e5f5505921b85a84
alanloffler <alanmatiasloffler@gmail.com>
2026-02-21 22:23:10 -0300

    refactor(permission): router group (protected)

--- internal/permission/permission_routes.go
+++ internal/permission/permission_routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(router *gin.Engine, q *sqlc.Queries) {
+func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var repo *PermissionRepository = NewPermissionRepository(q)
 	var handler *PermissionHandler = NewPermissionHandler(repo)
 	var permissions *gin.RouterGroup = router.Group("/permissions")
