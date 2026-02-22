e46e57e9f21e59c217126e3945d93ae87fbda1f6
alanloffler <alanmatiasloffler@gmail.com>
2026-02-21 22:02:36 -0300

    feat(business): permissions on routes

--- internal/business/business_routes.go
+++ internal/business/business_routes.go
@@ -2,6 +2,7 @@ package business
 
 import (
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/alanloffler/go-calth-api/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,9 +11,9 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var handler *BusinessHandler = NewBusinessHandler(repo)
 	var businesses *gin.RouterGroup = router.Group("/businesses")
 
-	businesses.POST("", handler.Create)
-	businesses.GET("", handler.GetAll)
-	businesses.GET("/:id", handler.GetOneByID)
-	businesses.PATCH("/:id", handler.Update)
-	businesses.DELETE("/:id", handler.Delete)
+	businesses.POST("", middleware.PermissionMiddleware(q, "business-create"), handler.Create)
+	businesses.GET("", middleware.PermissionMiddleware(q, "business-view"), handler.GetAll)
+	businesses.GET("/:id", middleware.PermissionMiddleware(q, "business-view"), handler.GetOneByID)
+	businesses.PATCH("/:id", middleware.PermissionMiddleware(q, "business-update"), handler.Update)
+	businesses.DELETE("/:id", middleware.PermissionMiddleware(q, "business-delete-hard"), handler.Delete)
 }
