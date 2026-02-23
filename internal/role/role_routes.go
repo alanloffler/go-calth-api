7193d1b373436ea64612244c5a70221743109214
alanloffler <alanmatiasloffler@gmail.com>
2026-02-23 11:56:34 -0300

    feat(role): PermissionMiddleware

--- internal/role/role_routes.go
+++ internal/role/role_routes.go
@@ -2,6 +2,7 @@ package role
 
 import (
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/alanloffler/go-calth-api/internal/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -11,13 +12,13 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries, pool *pgxpool.Pool
 	var handler *RoleHandler = NewRoleHandler(repo, pool)
 	var roles *gin.RouterGroup = router.Group("/roles")
 
-	roles.POST("", handler.Create)
-	roles.GET("", handler.GetAll)
-	roles.GET("/soft", handler.GetAllWithSoftDeleted)
-	roles.GET("/:id", handler.GetOneByID)
-	roles.GET("/:id/soft", handler.GetOneByIDWithSoftDeleted)
-	roles.PATCH("/:id", handler.Update)
-	roles.PATCH("/:id/restore", handler.Restore)
-	roles.DELETE("/:id", handler.Delete)
-	roles.DELETE("/:id/soft", handler.SoftDelete)
+	roles.POST("", middleware.PermissionMiddleware(q, "roles-create"), handler.Create)
+	roles.GET("", middleware.PermissionMiddleware(q, "roles-view"), handler.GetAll)
+	roles.GET("/soft", middleware.PermissionMiddleware(q, "roles-view"), handler.GetAllWithSoftDeleted)
+	roles.GET("/:id", middleware.PermissionMiddleware(q, "roles-view"), handler.GetOneByID)
+	roles.GET("/:id/soft", middleware.PermissionMiddleware(q, "roles-view"), handler.GetOneByIDWithSoftDeleted)
+	roles.PATCH("/:id", middleware.PermissionMiddleware(q, "roles-update"), handler.Update)
+	roles.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "roles-restore"), handler.Restore)
+	roles.DELETE("/:id", middleware.PermissionMiddleware(q, "roles-delete-hard"), handler.Delete)
+	roles.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "roles-delete"), handler.SoftDelete)
 }
