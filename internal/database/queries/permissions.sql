e2e0ad901d0c0a32cbcc3de14cbc2131678fbfc3
alanloffler <alanmatiasloffler@gmail.com>
2026-02-23 12:56:50 -0300

    refactor(database): permissions queries formatted

--- internal/database/queries/permissions.sql
+++ internal/database/queries/permissions.sql
@@ -1,52 +1,90 @@
 -- name: CreatePermission :one
-INSERT INTO permissions (
-    name, category, action_key, description
-) VALUES (
-    $1, $2, $3, $4
-) RETURNING *;
+INSERT INTO
+  permissions (name, category, action_key, description)
+VALUES
+  ($1, $2, $3, $4)
+RETURNING
+  *;
 
 -- name: GetPermissions :many
-SELECT * FROM permissions
-WHERE deleted_at IS NULL
-ORDER BY action_key ASC;
+SELECT
+  *
+FROM
+  permissions
+ORDER BY
+  action_key ASC;
 
 -- name: GetPermissionsWithSoftDeleted :many
-SELECT * FROM permissions
-ORDER BY action_key ASC;
+SELECT
+  *
+FROM
+  permissions
+ORDER BY
+  action_key ASC;
 
 -- name: GetPermissionsByCategory :many
-SELECT * FROM permissions
-WHERE category = $1
-ORDER BY action_key ASC;
+SELECT
+  *
+FROM
+  permissions
+WHERE
+  category = $1
+ORDER BY
+  action_key ASC;
 
 -- name: GetPermission :one
-SELECT * FROM permissions
-WHERE id = $1 AND deleted_at IS NULL;
+SELECT
+  *
+FROM
+  permissions
+WHERE
+  id = $1
+  AND deleted_at IS NULL;
 
 -- name: GetPermissionWithSoftDeleted :one
-SELECT * FROM permissions
-WHERE id = $1;
+SELECT
+  *
+FROM
+  permissions
+WHERE
+  id = $1;
 
 -- name: UpdatePermission :one
-UPDATE permissions SET
-  name = COALESCE(sqlc.narg('name'), name),
-  category = COALESCE(sqlc.narg('category'), category),
-  action_key = COALESCE(sqlc.narg('action_key'), action_key),
-  description = COALESCE(sqlc.narg('description'), description),
+UPDATE permissions
+SET
+  name = COALESCE(sqlc.narg ('name'), name),
+  category = COALESCE(sqlc.narg ('category'), category),
+  action_key = COALESCE(sqlc.narg ('action_key'), action_key),
+  description = COALESCE(sqlc.narg ('description'), description),
   updated_at = now()
-WHERE id = $1 AND deleted_at IS NULL
-RETURNING *;
+WHERE
+  id = $1
+  AND deleted_at IS NULL
+RETURNING
+  *;
 
 -- name: DeletePermission :exec
 DELETE FROM permissions
-WHERE id = $1 AND deleted_at IS NULL;
+WHERE
+  id = $1
+  AND deleted_at IS NULL;
 
 -- name: SoftDeletePermission :execrows
-UPDATE permissions SET deleted_at = now()
-WHERE id = $1 AND deleted_at IS NULL
-RETURNING *;
+UPDATE permissions
+SET
+  deleted_at = now()
+WHERE
+  id = $1
+  AND deleted_at IS NULL
+RETURNING
+  *;
 
 -- name: RestorePermission :execrows
-UPDATE permissions SET deleted_at = NULL
-WHERE id = $1 AND deleted_at IS NOT NULL
-RETURNING *;
+UPDATE permissions
+SET
+  deleted_at = NULL
+WHERE
+  id = $1
+  AND deleted_at IS NOT NULL
+RETURNING
+  *;
