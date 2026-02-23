26be1f9c23cdb9840da3b4fdcffc5b4f632455bd
alanloffler <alanmatiasloffler@gmail.com>
2026-02-23 12:05:12 -0300

    feat(permission): PermissionMiddleware

--- internal/permission/permission_routes.go
+++ internal/permission/permission_routes.go
@@ -2,6 +2,7 @@ package permission
 
 import (
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/alanloffler/go-calth-api/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,15 +11,15 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var handler *PermissionHandler = NewPermissionHandler(repo)
 	var permissions *gin.RouterGroup = router.Group("/permissions")
 
-	permissions.POST("", handler.Create)
-	permissions.GET("", handler.GetAll)
-	permissions.GET("/soft", handler.GetAllWithSoftDeleted)
-	permissions.GET("/category/:category", handler.GetAllByCategory)
-	permissions.GET("/grouped", handler.GetAllGrouped)
-	permissions.GET("/:id", handler.GetOneByID)
-	permissions.GET("/:id/soft", handler.GetOneByIDWithSoftDeleted)
-	permissions.PATCH("/:id", handler.Update)
-	permissions.PATCH("/:id/restore", handler.Restore)
-	permissions.DELETE("/:id", handler.Delete)
-	permissions.DELETE("/:id/soft", handler.SoftDelete)
+	permissions.POST("", middleware.PermissionMiddleware(q, "permissions-create"), handler.Create)
+	permissions.GET("", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAll)
+	permissions.GET("/soft", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAllWithSoftDeleted)
+	permissions.GET("/category/:category", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAllByCategory)
+	permissions.GET("/grouped", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAllGrouped)
+	permissions.GET("/:id", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetOneByID)
+	permissions.GET("/:id/soft", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetOneByIDWithSoftDeleted)
+	permissions.PATCH("/:id", middleware.PermissionMiddleware(q, "permissions-update"), handler.Update)
+	permissions.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "permissions-restore"), handler.Restore)
+	permissions.DELETE("/:id", middleware.PermissionMiddleware(q, "permissions-delete"), handler.Delete)
+	permissions.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "permissions-delete-hard"), handler.SoftDelete)
 }
