f0c7855b4cdcb028137edecb1870c06eecd69a6d
alanloffler <alanmatiasloffler@gmail.com>
2026-02-24 19:58:28 -0300

    feat(database): GetPatientProfileByUserID query

--- internal/database/queries/patient_profile.sql
+++ internal/database/queries/patient_profile.sql
@@ -1,8 +1,27 @@
 -- name: CreatePatientProfile :one
-INSERT INTO patient_profile (
-    business_id, user_id, gender, birth_day, blood_type,
-    weight, height, emergency_contact_name, emergency_contact_phone
-) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9
-)
-RETURNING *;
+INSERT INTO
+  patient_profile (
+    business_id,
+    user_id,
+    gender,
+    birth_day,
+    blood_type,
+    weight,
+    height,
+    emergency_contact_name,
+    emergency_contact_phone
+  )
+VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+RETURNING
+  *;
+
+-- name: GetPatientProfileByUserID :one
+SELECT
+  *
+FROM
+  patient_profile
+WHERE
+  business_id = $1
+  AND user_id = $2
+  AND deleted_at IS NULL;
