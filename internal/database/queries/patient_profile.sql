9bcb284a138fe67397c44c3bcf604354373bed81
alanloffler <alanmatiasloffler@gmail.com>
2026-02-24 20:13:08 -0300

    feat(database): UpdatePatientProfile query

--- internal/database/queries/patient_profile.sql
+++ internal/database/queries/patient_profile.sql
@@ -25,3 +25,21 @@ WHERE
   business_id = $1
   AND user_id = $2
   AND deleted_at IS NULL;
+
+-- name: UpdatePatientProfile :one
+UPDATE patient_profile
+SET
+  gender = $3,
+  birth_day = $4,
+  blood_type = $5,
+  weight = $6,
+  height = $7,
+  emergency_contact_name = $8,
+  emergency_contact_phone = $9,
+  updated_at = now()
+WHERE
+  business_id = $1
+  AND user_id = $2
+  AND deleted_at IS NULL
+RETURNING
+  *;
