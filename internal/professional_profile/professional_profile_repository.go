f27a2af33abb1a996761f8272acaf2949420be49
alanloffler <alanmatiasloffler@gmail.com>
2026-02-25 12:55:33 -0300

    feat(professional_profile): Create method on repository

---
+++ internal/professional_profile/professional_profile_repository.go
@@ -0,0 +1,19 @@
+package professional_profile
+
+import (
+	"context"
+
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+)
+
+type ProfessionalProfileRepository struct {
+	q *sqlc.Queries
+}
+
+func NewProfessionalProfileRepository(q *sqlc.Queries) *ProfessionalProfileRepository {
+	return &ProfessionalProfileRepository{q: q}
+}
+
+func (r *ProfessionalProfileRepository) Create(ctx context.Context, arg sqlc.CreateProfessionalProfileParams) (sqlc.ProfessionalProfile, error) {
+	return r.q.CreateProfessionalProfile(ctx, arg)
+}
