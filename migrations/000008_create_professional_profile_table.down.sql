b297b53a05d6812172aceed261f5455ee95b4054
alanloffler <alanmatiasloffler@gmail.com>
2026-02-25 16:39:08 -0300

    feat(migrations): index on professional_profile table

--- migrations/000008_create_professional_profile_table.down.sql
+++ migrations/000008_create_professional_profile_table.down.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS professional_profile;
+
+DROP INDEX IF EXISTS uq_prof_profile_business_user;
--- migrations/000008_create_professional_profile_table.up.sql
+++ migrations/000008_create_professional_profile_table.up.sql
@@ -14,7 +14,6 @@ CREATE TABLE professional_profile (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   deleted_at TIMESTAMPTZ,
-
   CONSTRAINT uq_prof_profile_business_license UNIQUE (business_id, license_id),
   CONSTRAINT fk_professional_profile_business FOREIGN KEY (business_id) REFERENCES businesses (id) ON DELETE CASCADE,
   CONSTRAINT fk_professional_profile_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
@@ -23,3 +22,5 @@ CREATE TABLE professional_profile (
 CREATE INDEX idx_prof_profile_business ON professional_profile (business_id);
 
 CREATE INDEX idx_prof_profile_business_user ON professional_profile (business_id, user_id);
+
+CREATE INDEX uq_prof_profile_business_user ON professional_profile (business_id, user_id);
