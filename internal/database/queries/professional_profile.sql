f708e12a0e8e07a41425831426bc600e1164b57d
alanloffler <alanmatiasloffler@gmail.com>
2026-02-25 21:01:55 -0300

    feat(database): GetProfessionalProfileByUserID query

--- internal/database/queries/professional_profile.sql
+++ internal/database/queries/professional_profile.sql
@@ -17,3 +17,13 @@ VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
 RETURNING
   *;
+
+-- name: GetProfessionalProfileByUserID :one
+SELECT
+  *
+FROM
+  professional_profile
+WHERE
+  business_id = $1
+  AND user_id = $2
+  AND deleted_at IS NULL;
