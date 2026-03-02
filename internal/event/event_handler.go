3ab5e6ca9cd1306523de5f641f1c15c668f620c9
alanloffler <alanmatiasloffler@gmail.com>
2026-03-01 22:20:23 -0300

    feat(event): Create method

--- internal/event/event_handler.go
+++ internal/event/event_handler.go
@@ -1,9 +1,76 @@
 package event
 
+import (
+	"net/http"
+	"time"
+
+	"github.com/alanloffler/go-calth-api/internal/common/response"
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
 type EventHandler struct {
 	repo *EventRepository
 }
 
+type CreateEventRequest struct {
+	Title          string `json:"title" binding:"required,min=3,max=255"`
+	StartDate      string `json:"start_date" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	EndDate        string `json:"end_date" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	ProfessionalID string `json:"professional_id" binding:"required,uuid"`
+	UserID         string `json:"user_id" binding:"required,uuid"`
+}
+
 func NewEventHandler(repo *EventRepository) *EventHandler {
 	return &EventHandler{repo: repo}
+}
+
+func (h *EventHandler) Create(c *gin.Context) {
+	var req CreateEventRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(http.StatusBadRequest, "Error de validación de datos", err))
+		return
+	}
+
+	startTime, err := time.Parse(time.RFC3339, req.StartDate)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(http.StatusBadRequest, "Formato de fecha de inicio inválido", err))
+		return
+	}
+
+	endTime, err := time.Parse(time.RFC3339, req.EndDate)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(http.StatusBadRequest, "Formato de fecha de finalización inválido", err))
+		return
+	}
+
+	startDate := pgtype.Timestamptz{Time: startTime, Valid: true}
+	endDate := pgtype.Timestamptz{Time: endTime, Valid: true}
+
+	var professionalID pgtype.UUID
+	if err := professionalID.Scan(req.ProfessionalID); err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(http.StatusBadRequest, "Formato de ID del profesional inválido", err))
+		return
+	}
+
+	var userID pgtype.UUID
+	if err := userID.Scan(req.UserID); err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(http.StatusBadRequest, "Formato de ID del usuario inválido", err))
+		return
+	}
+
+	event, err := h.repo.Create(c.Request.Context(), sqlc.CreateEventParams{
+		Title:          req.Title,
+		StartDate:      startDate,
+		EndDate:        endDate,
+		ProfessionalID: professionalID,
+		UserID:         userID,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.Error(http.StatusInternalServerError, "Error al crear evento", err))
+		return
+	}
+
+	c.JSON(http.StatusOK, response.Created("Evento creado", &event))
 }
