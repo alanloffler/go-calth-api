3a83578fe82c778945f1257569bbd981de3e5730
alanloffler <alanmatiasloffler@gmail.com>
2026-03-01 22:20:08 -0300

    feat(event): Create method

--- internal/event/event_repository.go
+++ internal/event/event_repository.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"context"
+
 	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
 )
 
@@ -10,4 +12,8 @@ type EventRepository struct {
 
 func NewEventRepository(q *sqlc.Queries) *EventRepository {
 	return &EventRepository{q: q}
+}
+
+func (r *EventRepository) Create(ctx context.Context, arg sqlc.CreateEventParams) (sqlc.Event, error) {
+	return r.q.CreateEvent(ctx, arg)
 }
