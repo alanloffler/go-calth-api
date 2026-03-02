cd191a7232e59ac339717714754f4a90b2af5663
alanloffler <alanmatiasloffler@gmail.com>
2026-03-02 15:20:29 -0300

    refactor(migrations): create events table extended down

--- migrations/000010_create_events_table.down.sql
+++ migrations/000010_create_events_table.down.sql
@@ -1,1 +1,9 @@
 DROP TABLE IF EXISTS events;
+
+DROP TYPE IF EXISTS event_status;
+
+DROP INDEX IF EXISTS idx_events_business_start;
+
+DROP INDEX IF EXISTS idx_events_business_professional_start;
+
+DROP INDEX IF EXISTS idx_events_business_user_start;
