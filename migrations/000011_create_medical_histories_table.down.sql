2a456581e16a4d8b6c342b8613e06de1a6a11287
alanloffler <alanmatiasloffler@gmail.com>
2026-03-04 11:43:03 -0300

    feat(migrations): create medical histories table down

---
+++ migrations/000011_create_medical_histories_table.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS medical_histories;
+
+DROP INDEX IF EXISTS idx_mh_business_user;
+
+DROP INDEX IF EXISTS idx_mh_business_event;
+
+DROP INDEX IF EXISTS idx_mh_business_user_created;
