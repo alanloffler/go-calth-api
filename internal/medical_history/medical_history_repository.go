39ef5aca63931ef32371dc46d222d52ea36ad005
alanloffler <alanmatiasloffler@gmail.com>
2026-03-04 19:08:08 -0300

    feat(medical_history): GetAllByPatientIDWithSoftDeleted method

--- internal/medical_history/medical_history_repository.go
+++ internal/medical_history/medical_history_repository.go
@@ -17,3 +17,7 @@ func NewMedicalHistoryRepository(q *sqlc.Queries) *MedicalHistoryRepository {
 func (r *MedicalHistoryRepository) CreateMedicaHistory(ctx context.Context, arg sqlc.CreateMedicalHistoryParams) (sqlc.MedicalHistory, error) {
 	return r.q.CreateMedicalHistory(ctx, arg)
 }
+
+func (r *MedicalHistoryRepository) GetAllByPatientIDWithSoftDeleted(ctx context.Context, arg sqlc.GetMedicalHistoriesByPatientIDWithSoftDeletedParams) ([]sqlc.GetMedicalHistoriesByPatientIDWithSoftDeletedRow, error) {
+	return r.q.GetMedicalHistoriesByPatientIDWithSoftDeleted(ctx, arg)
+}
