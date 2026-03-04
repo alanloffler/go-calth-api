d7a204414a2099bfd5763029d226b72d620ed3c4
alanloffler <alanmatiasloffler@gmail.com>
2026-03-04 19:08:44 -0300

    feat(medical_history): GetAllByPatientIDWithSoftDeleted route

--- internal/medical_history/medical_history_routes.go
+++ internal/medical_history/medical_history_routes.go
@@ -12,4 +12,6 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var medical_histories *gin.RouterGroup = router.Group("/medical-history")
 
 	medical_histories.POST("", middleware.PermissionMiddleware(q, "medical_history-create"), handler.CreateMedicalHistory)
+
+	medical_histories.GET("/:id/patient/removed", middleware.PermissionMiddleware(q, "medical_history-view"), handler.GetAllByPatientIDWithSoftDeleted)
 }
