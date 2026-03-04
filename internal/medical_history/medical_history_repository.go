7626562543f52c18fe8bd8f1bc678281b8c4cddc
alanloffler <alanmatiasloffler@gmail.com>
2026-03-04 17:23:07 -0300

    feat(medical_history): CreateMedicalHistory method

--- internal/medical_history/medical_history_repository.go
+++ internal/medical_history/medical_history_repository.go
@@ -1,6 +1,10 @@
 package medical_history
 
-import "github.com/alanloffler/go-calth-api/internal/database/sqlc"
+import (
+	"context"
+
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+)
 
 type MedicalHistoryRepository struct {
 	q *sqlc.Queries
@@ -8,4 +12,8 @@ type MedicalHistoryRepository struct {
 
 func NewMedicalHistoryRepository(q *sqlc.Queries) *MedicalHistoryRepository {
 	return &MedicalHistoryRepository{q: q}
+}
+
+func (r *MedicalHistoryRepository) CreateMedicaHistory(ctx context.Context, arg sqlc.CreateMedicalHistoryParams) (sqlc.MedicalHistory, error) {
+	return r.q.CreateMedicalHistory(ctx, arg)
 }
