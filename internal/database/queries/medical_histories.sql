d92ef2554d907f79f114f543c9b02e8b0f61862f
alanloffler <alanmatiasloffler@gmail.com>
2026-03-04 19:07:48 -0300

    feat(database): GetMedicalHistoriesByPatientIDWithSoftDeleted query

--- internal/database/queries/medical_histories.sql
+++ internal/database/queries/medical_histories.sql
@@ -14,3 +14,23 @@ VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING
   *;
+
+-- name: GetMedicalHistoriesByPatientIDWithSoftDeleted :many
+SELECT
+  mh.*,
+  u.ic,
+  u.first_name,
+  u.last_name,
+  p.first_name,
+  p.last_name,
+  pp.professional_prefix
+FROM
+  medical_histories mh
+  LEFT JOIN users u ON u.id = mh.user_id
+  LEFT JOIN users p ON p.id = mh.professional_id
+  LEFT JOIN professional_profile pp ON pp.user_id = p.id
+WHERE
+  mh.business_id = $1
+  AND mh.user_id = $2
+ORDER BY
+  mh.date DESC;
