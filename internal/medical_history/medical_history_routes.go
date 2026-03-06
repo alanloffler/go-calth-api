ef60092fc01be8c165910d047e48593550cf5915
alanloffler <alanmatiasloffler@gmail.com>
2026-03-06 11:16:48 -0300

    feat(medical_history): Restore route

--- internal/medical_history/medical_history_routes.go
+++ internal/medical_history/medical_history_routes.go
@@ -12,9 +12,8 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var medical_histories *gin.RouterGroup = router.Group("/medical-history")
 
 	medical_histories.POST("", middleware.PermissionMiddleware(q, "medical_history-create"), handler.CreateMedicalHistory)
-
 	medical_histories.GET("/:id/patient/removed", middleware.PermissionMiddleware(q, "medical_history-view"), handler.GetAllByPatientIDWithSoftDeleted)
 	medical_histories.GET("/:id/patient", middleware.PermissionMiddleware(q, "medical_history-view"), handler.GetAllByPatientIDWithSoftDeleted)
-
+	medical_histories.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "medical_history-restore"), handler.Restore)
 	medical_histories.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "medical_history-delete"), handler.SoftDelete)
 }
