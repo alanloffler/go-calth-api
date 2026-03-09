83c4e4a5026f635bb3f94386186160bcbd5dd028
alanloffler <alanmatiasloffler@gmail.com>
2026-03-09 19:59:29 -0300

    todo(event): create findEventsFiltered route

--- internal/event/event_routes.go
+++ internal/event/event_routes.go
@@ -20,6 +20,8 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetProfessionalEventsByDayArray)
 	events.GET("/:id", middleware.PermissionMiddleware(q, "events-view"), handler.GetByID)
 
+	// TODO: create findEventsFiltered -> already at Nest.js API
+
 	events.PATCH("/:id/status", middleware.PermissionMiddleware(q, "events-update"), handler.UpdateEventStatus)
 	events.PATCH("/:id", middleware.PermissionMiddleware(q, "events-update"), handler.UpdateEvent)
 }
