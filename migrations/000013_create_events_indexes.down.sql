80f0701fcaf0036ae994a34fe340825e7d3ea4b6
alanloffler <alanmatiasloffler@gmail.com>
2026-03-12 19:56:48 -0300

    feat(migrations): create indexes on events table down

---
+++ migrations/000013_create_events_indexes.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_events_business_status_start;
+
+DROP INDEX IF EXISTS idx_events_business_title;
+
+DROP INDEX IF EXISTS idx_events_not_deleted;
+
+DROP INDEX IF EXISTS idx_users_first_name;
