f83865cbe98cd0d462f845c9a5bf47a0cc8d3862
alanloffler <alanmatiasloffler@gmail.com>
2026-03-15 17:34:20 -0300

    feat(event): GetDaysWithEvents route

--- internal/event/event_routes.go
+++ internal/event/event_routes.go
@@ -16,6 +16,7 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	events.GET("/professional/:id", middleware.PermissionMiddleware(q, "events-view"), handler.GetByProfessionalID)
 	events.GET("/business", middleware.PermissionMiddleware(q, "events-view"), handler.GetByBusinessID)
 	events.GET("/filtered", middleware.PermissionMiddleware(q, "events-view"), handler.GetEventsFiltered)
+	events.GET("/days-with-events/:id", middleware.PermissionMiddleware(q, "events-view"), handler.GetDaysWithEvents)
 	// events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetProfessionalEventsByDay)
 	events.GET("/patient/:patient_id", middleware.PermissionMiddleware(q, "events-view"), handler.GetEventsByBusinessProfessionalPatient)
 	events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetProfessionalEventsByDayArray)
