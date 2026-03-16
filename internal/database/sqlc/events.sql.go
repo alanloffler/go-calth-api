346632f58738ae29f6cadde796ad83f8336efed6
alanloffler <alanmatiasloffler@gmail.com>
2026-03-16 19:30:52 -0300

    refactor(database): sqlc rename get by business id

--- internal/database/sqlc/events.sql.go
+++ internal/database/sqlc/events.sql.go
@@ -62,6 +62,99 @@ func (q *Queries) Create(ctx context.Context, arg CreateParams) (Event, error) {
 	return i, err
 }
 
+const getByBusinessID = `-- name: GetByBusinessID :many
+SELECT
+  jsonb_build_object(
+    'id',
+    e.id,
+    'title',
+    e.title,
+    'startDate',
+    e.start_date,
+    'endDate',
+    e.end_date,
+    'businessId',
+    e.business_id,
+    'userId',
+    e.user_id,
+    'status',
+    e.status,
+    'createdAt',
+    e.created_at,
+    'updatedAt',
+    e.updated_at,
+    'professional',
+    jsonb_build_object(
+      'id',
+      p.id,
+      'firstName',
+      p.first_name,
+      'lastName',
+      p.last_name,
+      'ic',
+      p.ic,
+      'professionalProfile',
+      jsonb_build_object('professionalPrefix', pp.professional_prefix)
+    ),
+    'user',
+    jsonb_build_object(
+      'id',
+      u.id,
+      'firstName',
+      u.first_name,
+      'lastName',
+      u.last_name,
+      'email',
+      u.email,
+      'phoneNumber',
+      u.phone_number,
+      'ic',
+      u.ic,
+      'role',
+      jsonb_build_object('name', ur.name, 'value', ur.value)
+    )
+  ) AS event
+FROM
+  events e
+  LEFT JOIN users u ON u.id = e.user_id
+  LEFT JOIN roles ur ON ur.id = u.role_id
+  LEFT JOIN users p ON p.id = e.professional_id
+  LEFT JOIN professional_profile pp ON pp.user_id = p.id
+WHERE
+  e.business_id = $1
+  AND e.deleted_at IS NULL
+ORDER BY
+  e.start_date::date DESC,
+  e.end_date::time DESC
+LIMIT
+  $2
+`
+
+type GetByBusinessIDParams struct {
+	BusinessID pgtype.UUID `json:"businessId"`
+	Limit      int32       `json:"limit"`
+}
+
+func (q *Queries) GetByBusinessID(ctx context.Context, arg GetByBusinessIDParams) ([][]byte, error) {
+	rows, err := q.db.Query(ctx, getByBusinessID, arg.BusinessID, arg.Limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items [][]byte
+	for rows.Next() {
+		var event []byte
+		if err := rows.Scan(&event); err != nil {
+			return nil, err
+		}
+		items = append(items, event)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const getByID = `-- name: GetByID :one
 SELECT
   jsonb_build_object(
@@ -374,99 +467,6 @@ func (q *Queries) GetDaysWithEvents(ctx context.Context, arg GetDaysWithEventsPa
 			return nil, err
 		}
 		items = append(items, day)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return items, nil
-}
-
-const getEventsByBusinessID = `-- name: GetEventsByBusinessID :many
-SELECT
-  jsonb_build_object(
-    'id',
-    e.id,
-    'title',
-    e.title,
-    'startDate',
-    e.start_date,
-    'endDate',
-    e.end_date,
-    'businessId',
-    e.business_id,
-    'userId',
-    e.user_id,
-    'status',
-    e.status,
-    'createdAt',
-    e.created_at,
-    'updatedAt',
-    e.updated_at,
-    'professional',
-    jsonb_build_object(
-      'id',
-      p.id,
-      'firstName',
-      p.first_name,
-      'lastName',
-      p.last_name,
-      'ic',
-      p.ic,
-      'professionalProfile',
-      jsonb_build_object('professionalPrefix', pp.professional_prefix)
-    ),
-    'user',
-    jsonb_build_object(
-      'id',
-      u.id,
-      'firstName',
-      u.first_name,
-      'lastName',
-      u.last_name,
-      'email',
-      u.email,
-      'phoneNumber',
-      u.phone_number,
-      'ic',
-      u.ic,
-      'role',
-      jsonb_build_object('name', ur.name, 'value', ur.value)
-    )
-  ) AS event
-FROM
-  events e
-  LEFT JOIN users u ON u.id = e.user_id
-  LEFT JOIN roles ur ON ur.id = u.role_id
-  LEFT JOIN users p ON p.id = e.professional_id
-  LEFT JOIN professional_profile pp ON pp.user_id = p.id
-WHERE
-  e.business_id = $1
-  AND e.deleted_at IS NULL
-ORDER BY
-  e.start_date::date DESC,
-  e.end_date::time DESC
-LIMIT
-  $2
-`
-
-type GetEventsByBusinessIDParams struct {
-	BusinessID pgtype.UUID `json:"businessId"`
-	Limit      int32       `json:"limit"`
-}
-
-func (q *Queries) GetEventsByBusinessID(ctx context.Context, arg GetEventsByBusinessIDParams) ([][]byte, error) {
-	rows, err := q.db.Query(ctx, getEventsByBusinessID, arg.BusinessID, arg.Limit)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	var items [][]byte
-	for rows.Next() {
-		var event []byte
-		if err := rows.Scan(&event); err != nil {
-			return nil, err
-		}
-		items = append(items, event)
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
