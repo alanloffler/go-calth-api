5b8bfc555750551c3455706b71693e0c0369e3b3
alanloffler <alanmatiasloffler@gmail.com>
2026-03-16 19:40:22 -0300

    refactor(event): rename to get by professional day handler on route

--- internal/event/event_routes.go
+++ internal/event/event_routes.go
@@ -17,7 +17,7 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	events.GET("/business", middleware.PermissionMiddleware(q, "events-view"), handler.GetByBusinessID)
 	events.GET("/filtered", middleware.PermissionMiddleware(q, "events-view"), handler.GetFiltered)
 	events.GET("/days-with-events/:id", middleware.PermissionMiddleware(q, "events-view"), handler.GetDaysWithEvents)
-	// events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetProfessionalEventsByDay)
+	// events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetByProfessionalDay)
 	events.GET("/patient/:patient_id", middleware.PermissionMiddleware(q, "events-view"), handler.GetByBusinessProfessionalPatient)
 	events.GET("/professional/:id/date-array/:day", middleware.PermissionMiddleware(q, "events-view"), handler.GetProfessionalEventsByDayArray)
 	events.GET("/:id", middleware.PermissionMiddleware(q, "events-view"), handler.GetByID)
