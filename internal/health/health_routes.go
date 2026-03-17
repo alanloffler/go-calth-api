3f96b8d1270cfaf2e56e6fbf30bf01c9c8265ef4
alanloffler <alanmatiasloffler@gmail.com>
2026-03-17 17:10:39 -0300

    refactor(health): messages to spanish

--- internal/health/health_routes.go
+++ internal/health/health_routes.go
@@ -12,15 +12,15 @@ func RegisterRoutes(router *gin.Engine, pool *pgxpool.Pool) {
 	var health *gin.RouterGroup = router.Group("/health")
 
 	health.GET("", func(c *gin.Context) {
-		c.JSON(http.StatusOK, response.Success[any]("Calth API running", nil))
+		c.JSON(http.StatusOK, response.Success[any]("Calth API en ejecución", nil))
 	})
 
 	health.GET("/db", func(c *gin.Context) {
 		if err := pool.Ping(c.Request.Context()); err != nil {
-			c.JSON(http.StatusInternalServerError, response.Error(http.StatusInternalServerError, "Database is not connected", err))
+			c.JSON(http.StatusInternalServerError, response.Error(http.StatusInternalServerError, "La base de datos no está conectada", err))
 			return
 		}
 
-		c.JSON(http.StatusOK, response.Success[any]("Database connected successfully", nil))
+		c.JSON(http.StatusOK, response.Success[any]("Base de datos conectada exitosamente", nil))
 	})
 }
