f7981d75aa092a485365b514fb9957106d7138f4
alanloffler <alanmatiasloffler@gmail.com>
2026-03-17 17:17:47 -0300

    refactor(database): format

--- internal/database/queries/role_permissions.sql
+++ internal/database/queries/role_permissions.sql
@@ -1,18 +1,25 @@
 -- name: CreateRolePermission :one
-INSERT INTO role_permissions (
-    role_id, permission_id
-)
-VALUES (
-    $1, $2
-)
-RETURNING *;
+INSERT INTO
+  role_permissions (role_id, permission_id)
+VALUES
+  ($1, $2)
+RETURNING
+  *;
 
 -- name: DeleteRolePermissionsByRoleID :exec
-DELETE FROM role_permissions WHERE role_id = $1;
+DELETE FROM role_permissions
+WHERE
+  role_id = $1;
 
 -- name: HasPermission :one
-SELECT EXISTS (
-    SELECT 1 FROM role_permissions rp
-    JOIN permissions p ON p.id = rp.permission_id
-    WHERE rp.role_id = $1 AND p.action_key = $2
-) AS has_permission;
+SELECT
+  EXISTS (
+    SELECT
+      1
+    FROM
+      role_permissions rp
+      JOIN permissions p ON p.id = rp.permission_id
+    WHERE
+      rp.role_id = $1
+      AND p.action_key = $2
+  ) AS has_permission;
