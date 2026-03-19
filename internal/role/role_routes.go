4ebe5ee844f58d91bd72b625ea115185cdb12e73
alanloffler <alanmatiasloffler@gmail.com>
2026-03-19 16:06:47 -0300

    refactor(role): grouping format

--- internal/role/role_routes.go
+++ internal/role/role_routes.go
@@ -13,12 +13,15 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries, pool *pgxpool.Pool
 	var roles *gin.RouterGroup = router.Group("/roles")
 
 	roles.POST("", middleware.PermissionMiddleware(q, "roles-create"), handler.Create)
+
 	roles.GET("", middleware.PermissionMiddleware(q, "roles-view"), handler.GetAll)
 	roles.GET("/soft", middleware.PermissionMiddleware(q, "roles-view"), handler.GetAllWithSoftDeleted)
 	roles.GET("/:id", middleware.PermissionMiddleware(q, "roles-view"), handler.GetOneByID)
 	roles.GET("/:id/soft", middleware.PermissionMiddleware(q, "roles-view"), handler.GetOneByIDWithSoftDeleted)
+
 	roles.PATCH("/:id", middleware.PermissionMiddleware(q, "roles-update"), handler.Update)
 	roles.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "roles-restore"), handler.Restore)
+
 	roles.DELETE("/:id", middleware.PermissionMiddleware(q, "roles-delete-hard"), handler.Delete)
 	roles.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "roles-delete"), handler.SoftDelete)
 }
