54b3c7482f70c826e0761d902ccd6b0dc9f67da2
alanloffler <alanmatiasloffler@gmail.com>
2026-03-19 16:01:13 -0300

    refactor(permission): grouping format

--- internal/permission/permission_routes.go
+++ internal/permission/permission_routes.go
@@ -12,12 +12,15 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var permissions *gin.RouterGroup = router.Group("/permissions")
 
 	permissions.POST("", middleware.PermissionMiddleware(q, "permissions-create"), handler.Create)
+
 	permissions.GET("", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAll)
 	permissions.GET("/category/:category", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAllByCategory)
 	permissions.GET("/grouped", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetAllGrouped)
 	permissions.GET("/:id", middleware.PermissionMiddleware(q, "permissions-view"), handler.GetOneByID)
+
 	permissions.PATCH("/:id", middleware.PermissionMiddleware(q, "permissions-update"), handler.Update)
 	permissions.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "permissions-restore"), handler.Restore)
+
 	permissions.DELETE("/:id", middleware.PermissionMiddleware(q, "permissions-delete"), handler.Delete)
 	permissions.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "permissions-delete-hard"), handler.SoftDelete)
 }
