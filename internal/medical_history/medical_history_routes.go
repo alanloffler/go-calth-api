13edbdc76436d56f74b43619643e4bdd7ee929e2
alanloffler <alanmatiasloffler@gmail.com>
2026-03-19 15:26:58 -0300

    refactor(medical_history): grouping format

--- internal/medical_history/medical_history_routes.go
+++ internal/medical_history/medical_history_routes.go
@@ -12,10 +12,13 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var medical_histories *gin.RouterGroup = router.Group("/medical-history")
 
 	medical_histories.POST("", middleware.PermissionMiddleware(q, "medical_history-create"), handler.Create)
+
 	medical_histories.GET("/:id/patient/removed", middleware.PermissionMiddleware(q, "medical_history-view"), handler.GetAllByPatientIDWithSoftDeleted)
 	medical_histories.GET("/:id/patient", middleware.PermissionMiddleware(q, "medical_history-view"), handler.GetAllByPatientIDWithSoftDeleted)
+
 	medical_histories.PATCH("/:id", middleware.PermissionMiddleware(q, "medical_history-update"), handler.Update)
 	medical_histories.PATCH("/:id/restore", middleware.PermissionMiddleware(q, "medical_history-restore"), handler.Restore)
+
 	medical_histories.DELETE("/:id/soft", middleware.PermissionMiddleware(q, "medical_history-delete"), handler.SoftDelete)
 	medical_histories.DELETE("/:id", middleware.PermissionMiddleware(q, "medical_history-delete-hard"), handler.Delete)
 }
