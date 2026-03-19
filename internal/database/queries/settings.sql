1eeeb06edca2f405eeea75ea2c56228d0287da11
alanloffler <alanmatiasloffler@gmail.com>
2026-03-19 19:11:36 -0300

    feat(database): UpdateSetting query

---
+++ internal/database/queries/settings.sql
@@ -0,0 +1,11 @@
+-- name: UpdateSetting :execrows
+UPDATE settings
+SET
+  module = COALESCE(sqlc.narg ('module'), module),
+  submodule = COALESCE(sqlc.narg ('submodule'), submodule),
+  key = COALESCE(sqlc.narg ('key'), key),
+  value = COALESCE(sqlc.narg ('value'), value),
+  title = COALESCE(sqlc.narg ('title'), title),
+  updated_at = now()
+WHERE
+  id = $1;
