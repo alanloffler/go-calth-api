837269df63cbc6b127b7e0603e4cf2cdd268921f
alanloffler <alanmatiasloffler@gmail.com>
2026-03-22 18:49:19 -0300

    refactor(business): grouping format

--- internal/business/business_routes.go
+++ internal/business/business_routes.go
@@ -14,8 +14,11 @@ func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var businesses *gin.RouterGroup = router.Group("/businesses")
 
 	businesses.POST("", middleware.PermissionMiddleware(q, "business-create"), handler.Create)
+
 	businesses.GET("", middleware.PermissionMiddleware(q, "business-view"), handler.GetAll)
 	businesses.GET("/:id", middleware.PermissionMiddleware(q, "business-view"), handler.GetOneByID)
+
 	businesses.PATCH("/:id", middleware.PermissionMiddleware(q, "business-update"), handler.Update)
+
 	businesses.DELETE("/:id", middleware.PermissionMiddleware(q, "business-delete-hard"), handler.Delete)
 }
