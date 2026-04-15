f9f4a71d6bf76ed770ee2d9838fe80d0a70eb76a
alanloffler <alanmatiasloffler@gmail.com>
2026-04-15 19:20:20 -0300

    feat(queue): EventCreatedPayload type

--- internal/queue/types.go
+++ internal/queue/types.go
@@ -5,3 +5,10 @@ type BusinessCreatedPayload struct {
 	BusinessName string `json:"businessName"`
 	BusinessLink string `json:"businessLink"`
 }
+
+type EventCreatedPayload struct {
+	Email     string `json:"email"`
+	FullName  string `json:"fullName"`
+	Title     string `json:"title"`
+	StartDate string `json:"startDate"`
+}
