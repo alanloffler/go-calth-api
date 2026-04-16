7161763915c3246aeba48e09cdb3e363db1f6f4d
alanloffler <alanmatiasloffler@gmail.com>
2026-04-16 19:54:15 -0300

    fix(blocked-day): group path

--- internal/blocked-day/blocked-day_routes.go
+++ internal/blocked-day/blocked-day_routes.go
@@ -9,7 +9,7 @@ import (
 func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
 	var repo *BlockedDayRepository = NewBlockedDayRepository(q)
 	var handler *BlockedDayHandler = NewBlockedDayHandler(repo)
-	var blocked_days *gin.RouterGroup = router.Group("blocked-day")
+	var blocked_days *gin.RouterGroup = router.Group("blocked-days")
 
 	blocked_days.POST("", middleware.PermissionMiddleware(q, "patient-update"), handler.Create)
 }
