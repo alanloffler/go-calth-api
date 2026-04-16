3c76c2cbc4224d21e70bf198d9ef23b48e0c67b2
alanloffler <alanmatiasloffler@gmail.com>
2026-04-16 19:31:12 -0300

    feat(blocked-day): routes

---
+++ internal/blocked-day/blocked-day_routes.go
@@ -0,0 +1,15 @@
+package blocked_day
+
+import (
+	"github.com/alanloffler/go-calth-api/internal/database/sqlc"
+	"github.com/alanloffler/go-calth-api/internal/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.RouterGroup, q *sqlc.Queries) {
+	var repo *BlockedDayRepository = NewBlockedDayRepository(q)
+	var handler *BlockedDayHandler = NewBlockedDayHandler(repo)
+	var blocked_days *gin.RouterGroup = router.Group("blocked-day")
+
+	blocked_days.POST("", middleware.PermissionMiddleware(q, "patient-update"), handler.Create)
+}
